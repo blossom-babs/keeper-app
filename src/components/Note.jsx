2bc276e220fb3362548bca1fd7009e3aa8b319d0
Blossom <saphashb@gmail.com>
2023-02-01 19:46:56 +0100

    close the modal when it is out of focus

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,17 +3,19 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import ClickAwayListener from '@mui/base/ClickAwayListener';
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 400,
+	bgcolor: 'background.paper',
+	border: '2px solid #000',
+	boxShadow: 24,
+	p: 4,
+	outline: 0
 };
 
 const Note = (props) => {
@@ -22,31 +24,36 @@ const Note = (props) => {
 	const handleClose = () => setOpen(false);
 
 	return (
-		<div className="note" onClick={handleOpen}>
-			<h1>{props.title}</h1>
-			<p>{props.content}</p>
-			<button
-				onClick={() => {
-					props.delete(props.id);
-				}}>
-				<DeleteIcon />
-			</button>
-
-			<Modal
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="modal-modal-title"
-				aria-describedby="modal-modal-description">
-				<Box sx={style}>
-					<Typography id="modal-modal-title" variant="h6" component="h2">
-						Text in a modal
-					</Typography>
-					<Typography id="modal-modal-description" sx={{ mt: 2 }}>
-						Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-					</Typography>
-				</Box>
-			</Modal>
-		</div>
+		<ClickAwayListener onClickAway={handleClose}>
+			<div className="note">
+				<div onClick={handleOpen}>
+					Open note
+					<h1>{props.title}</h1>
+					<p>{props.content}</p>
+					<button
+						className="note-delete"
+						onClick={() => {
+							props.delete(props.id);
+						}}>
+						<DeleteIcon />
+					</button>
+					<Modal
+						open={open}
+						onClose={handleClose}
+						aria-labelledby="modal-modal-title"
+						aria-describedby="modal-modal-description">
+						<Box sx={style}>
+							<Typography id="modal-modal-title" variant="h6" component="h2">
+								Text in a modal
+							</Typography>
+							<Typography id="modal-modal-description" sx={{ mt: 2 }}>
+								Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+							</Typography>
+						</Box>
+					</Modal>
+				</div>
+			</div>
+		</ClickAwayListener>
 	);
 };
 
